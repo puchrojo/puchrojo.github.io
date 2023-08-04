06e959e315e037c7214fe87379f9f6b882dee8f9
Isaac Puch Rojo <puchrojo@gmail.com>
2023-08-04 14:48:50 +0200

    Rename book

--- autorun.sh
+++ autorun.sh
@@ -1,1 +1,9 @@
-find . -name '*.adoc' | entr docker run --rm -v $PWD:/documents/ asciidoctor/docker-asciidoctor asciidoctor-pdf -vwt -o output/mybook.pdf master.adoc 
+
+# pdf
+# find . -name '*.adoc' | entr docker run --rm -v $PWD:/documents/ asciidoctor/docker-asciidoctor asciidoctor-pdf -vwt -o output/mybook.pdf master.adoc
+
+# epub
+#find . -name '*.adoc' | entr docker run --rm -v $PWD:/documents/ asciidoctor/docker-asciidoctor asciidoctor-epub3 -vwt -o output/mybook.epub master.adoc
+
+# html
+find . -name '*.adoc' | entr docker run --rm -v $PWD:/documents/ asciidoctor/docker-asciidoctor asciidoctor-html -vwt -o output/mybook.html master.adoc 
